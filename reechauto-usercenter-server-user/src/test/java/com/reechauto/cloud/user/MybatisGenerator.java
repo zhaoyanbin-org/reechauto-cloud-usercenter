4e2bda39c6c8328198c79c7b79c47464d65bc630
jinglili <jinglili@lvchiauto.com>
2019-04-10 10:02:27 +0800

    ll

--- reechauto-usercenter-server-user/src/test/java/com/reechauto/cloud/user/MybatisGenerator.java
+++ reechauto-usercenter-server-user/src/test/java/com/reechauto/cloud/user/MybatisGenerator.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
-
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.internal.DefaultShellCallback;
+
 
 public class MybatisGenerator {
 	
